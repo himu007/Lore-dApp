66b07f893ba33f9d713272b5f49024b76b7c02a0
Cryptosammy <91264400+SamanthaLoeve@users.noreply.github.com>
2021-12-24 22:22:21 +0100

    Latest build-link

--- LoreDApp/lore-prepare.bat
+++ LoreDApp/lore-prepare.bat
@@ -3,6 +3,6 @@ npm install
 cd lore
 npm install
 cd public\Build
-wget https://www.dropbox.com/s/hpj6f23364letnr/lore_update_001.zip
+wget https://www.dropbox.com/s/7i4s2zhx6qrcyko/lore-performance-6.zip
 unzip lore_update_001.zip
 rm lore_update_001.zip
